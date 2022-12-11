5e033353fe67f3c01c9e7801813d9b31d9155e22
assefaakrem <assefaakrem@gmail.com>
2022-12-11 05:09:22 -0800

    ptint

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,7 @@
  *main-Entry point
  * Description: 'the program's description'
  *Return: Always 0 (Success)
- */
-/*program to output the script*/
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
