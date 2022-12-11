012040895c6e1d46b2af23e66113e9acf89ea4c9
assefaakrem <assefaakrem@gmail.com>
2022-12-11 07:11:04 -0800

    outputing alphabets

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,11 +8,11 @@
 */
 int main(void)
 {
-char low;
-for (low = 'a'; low <= 'z'; low++)
-	putchar(low);
-	printf("\n");
+	char low;
+	for (low = 'a'; low <= 'z'; low++)
+		putchar(low);
+		printf("\n");
 	
-return(0);
+	return(0);
 }
 
