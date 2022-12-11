d66cbd51fb7f6917374da97d1ef911701d4faaff
assefaakrem <assefaakrem@gmail.com>
2022-12-11 10:46:22 -0800

    combination

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	int ones= '0';
 	int tens= '0';
-	for (tens='0'; tens='9'; tens++)/*prints ten digit*/
+	for(tens='0'; tens='9'; tens++)/*prints ten digit*/
 	{
-		for (ones='0'; ones='9'; ones++)
+		for(ones='0'; ones='9'; ones++)
 		{
-			if (!((ones==tens) || (tens > ones)))/*eliminates repitition*/
+			if(!((ones==tens) || (tens > ones)))/*eliminates repitition*/
 			{
 				putchar(tens);
 				putchar(ones);
