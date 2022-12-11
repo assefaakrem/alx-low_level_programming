8f4b8d31c932644fd4e26ad58849b278a03099db
assefaakrem <assefaakrem@gmail.com>
2022-12-11 01:02:29 -0800

    prints the output

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -6,8 +6,8 @@
  *Return: Always 0 (Success)
 */
 int main(void)
-	/* function to print the output *\
 {
+	/* print the output*/
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 			return (0);
 }
