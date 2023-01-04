8ea18b2d2867c15edd001e3f8fe3db4a7fd85a2f
assefaakrem <assefaakrem@gmail.com>
2023-01-04 03:02:08 -0800

    adding wildcmp

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+int strlen_no_wilds(char *str);
+void iterate_wild(char **wildstr);
+char *postfix_max(char *str, char *postfix);
+int wildcmp(char *s1, char *s2);
+
+/**
+ * strlen_no_wilds- Returns the length of string ignoring wildcared characters
+ * @str- the string to be measured
+ *
+ * Return -the length
+ */
+
+int strlen_no_wilds(char *str)
+{
+	int len = 0, index = 0;
+}
