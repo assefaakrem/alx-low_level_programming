ff5474bccf0d415b809a21f20fe788ea4507fe35
assefaakrem <assefaakrem@gmail.com>
2023-01-17 00:43:25 -0800

    Define a new type struct dog

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -130,8 +130,6 @@ int main(int argc, char *argv[])
 		}
 		exit(98);
 	}
-	for (len1 = 0; argv[1][len1]; len1++);
-	for (len2 = 0; argv[2][len2]; len2++);
 	lenres = len1 + len2;
 	tabres = _calloc(lenres, sizeof(int));
 	if (tabres == NULL)
